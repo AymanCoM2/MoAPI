ab6077fc74f04e49b0f5ccfe112175094f926d8d
= <aymancoom3@gmail.com>
2023-11-13 16:05:58 +0200

    b

--- routes/api.php
+++ routes/api.php
@@ -64,45 +64,45 @@ Route::post('/verify', function (Request $request) {
 }); // * EndPoint#4
 
 // http://10.10.20.11:8000/api/f/last-five/0535575165
-Route::get('/f/last-five/{phoneNumber}', function (Request $request) {
-    // Standatd MOAPI 
-    $inputPhoneNumber = $request->phoneNumber;
-    $userEntries  = AlJouaiRequests::getAllCustomerDocEntries($inputPhoneNumber);
-    natsort($userEntries);
-    $reversed  = array_reverse($userEntries);
-    $lastFiveArray  = array_slice($reversed, 0, 5);
-    $userInvoicesDates  = AlJouaiRequests::getAllCustomerInvoicesDates($lastFiveArray);
-    $otherArray  = [];
-    foreach ($userInvoicesDates as $key => $value) {
-        $newKey  = $value['DocDate'];
-        if (isset($otherArray[$newKey])) {
-            $otherArray[$newKey][] = $key;
-        } else {
-            $kwys = [];
-            $kwys[] = $key;
-            $otherArray[$newKey]  = $kwys;
-        }
-    }
-    ksort($otherArray);
-    $newArrayForDate = [];
-    $newContainer = [];
-    foreach ($otherArray as $date => $arrOfEntries) {
-        foreach ($arrOfEntries as $index => $docEntry) {
-            $newArrayForDate[$docEntry] = [
-                'Total' => AlJouaiRequests::getInvoiceDocTotal($docEntry),
-                'NumberOfItems' => AlJouaiRequests::getCountOfNumbers($docEntry),
-                'Dates' => AlJouaiRequests::getInvoiceDatesOnly($docEntry),
-            ];
-            if (isset($newContainer[$date])) {
-                $newContainer[$date][$docEntry][] = $newArrayForDate[$docEntry];
-            } else {
-                $newContainer[$date] = [];
-                $newContainer[$date][$docEntry][] = $newArrayForDate[$docEntry];
-            }
-        }
-    }
-    return response()->json($newContainer); // Data is Now Sorted 
-}); // * DONE 
+// Route::get('/f/last-five/{phoneNumber}', function (Request $request) {
+//     // Standatd MOAPI 
+//     $inputPhoneNumber = $request->phoneNumber;
+//     $userEntries  = AlJouaiRequests::getAllCustomerDocEntries($inputPhoneNumber);
+//     natsort($userEntries);
+//     $reversed  = array_reverse($userEntries);
+//     $lastFiveArray  = array_slice($reversed, 0, 5);
+//     $userInvoicesDates  = AlJouaiRequests::getAllCustomerInvoicesDates($lastFiveArray);
+//     $otherArray  = [];
+//     foreach ($userInvoicesDates as $key => $value) {
+//         $newKey  = $value['DocDate'];
+//         if (isset($otherArray[$newKey])) {
+//             $otherArray[$newKey][] = $key;
+//         } else {
+//             $kwys = [];
+//             $kwys[] = $key;
+//             $otherArray[$newKey]  = $kwys;
+//         }
+//     }
+//     ksort($otherArray);
+//     $newArrayForDate = [];
+//     $newContainer = [];
+//     foreach ($otherArray as $date => $arrOfEntries) {
+//         foreach ($arrOfEntries as $index => $docEntry) {
+//             $newArrayForDate[$docEntry] = [
+//                 'Total' => AlJouaiRequests::getInvoiceDocTotal($docEntry),
+//                 'NumberOfItems' => AlJouaiRequests::getCountOfNumbers($docEntry),
+//                 'Dates' => AlJouaiRequests::getInvoiceDatesOnly($docEntry),
+//             ];
+//             if (isset($newContainer[$date])) {
+//                 $newContainer[$date][$docEntry][] = $newArrayForDate[$docEntry];
+//             } else {
+//                 $newContainer[$date] = [];
+//                 $newContainer[$date][$docEntry][] = $newArrayForDate[$docEntry];
+//             }
+//         }
+//     }
+//     return response()->json($newContainer); // Data is Now Sorted 
+// }); // * DONE 
 
 // Route::get('/test-test/{doc}', function (Request $request) {
 //     $doc = $request->doc  ; 
--- routes/apiFour.php
+++ routes/apiFour.php
@@ -15,40 +15,41 @@ Route::get('/test-test/{phoneNumber}', function (Request $request) {
 // http://10.10.20.11:8000/api/f/last-five/0535575165
 Route::get('/f/last-five/{phoneNumber}', function (Request $request) {
     // Standatd MOAPI 
-    $inputPhoneNumber = $request->phoneNumber;
-    $userEntries  = AlJouaiRequests::getAllCustomerDocEntries($inputPhoneNumber);
-    natsort($userEntries);
-    $reversed  = array_reverse($userEntries);
-    $lastFiveArray  = array_slice($reversed, 0, 5);
-    $userInvoicesDates  = AlJouaiRequests::getAllCustomerInvoicesDates($lastFiveArray);
-    $otherArray  = [];
-    foreach ($userInvoicesDates as $key => $value) {
-        $newKey  = $value['DocDate'];
-        if (isset($otherArray[$newKey])) {
-            $otherArray[$newKey][] = $key;
-        } else {
-            $kwys = [];
-            $kwys[] = $key;
-            $otherArray[$newKey]  = $kwys;
-        }
-    }
-    ksort($otherArray);
-    $newArrayForDate = [];
-    $newContainer = [];
-    foreach ($otherArray as $date => $arrOfEntries) {
-        foreach ($arrOfEntries as $index => $docEntry) {
-            $newArrayForDate[$docEntry] = [
-                'Total' => AlJouaiRequests::getInvoiceDocTotal($docEntry),
-                'NumberOfItems' => AlJouaiRequests::getCountOfNumbers($docEntry),
-                'Dates' => AlJouaiRequests::getInvoiceDatesOnly($docEntry),
-            ];
-            if (isset($newContainer[$date])) {
-                $newContainer[$date][$docEntry][] = $newArrayForDate[$docEntry];
-            } else {
-                $newContainer[$date] = [];
-                $newContainer[$date][$docEntry][] = $newArrayForDate[$docEntry];
-            }
-        }
-    }
-    return response()->json($newContainer); // Data is Now Sorted 
+    // $inputPhoneNumber = $request->phoneNumber;
+    // $userEntries  = AlJouaiRequests::getAllCustomerDocEntries($inputPhoneNumber);
+    // natsort($userEntries);
+    // $reversed  = array_reverse($userEntries);
+    // $lastFiveArray  = array_slice($reversed, 0, 5);
+    // $userInvoicesDates  = AlJouaiRequests::getAllCustomerInvoicesDates($lastFiveArray);
+    // $otherArray  = [];
+    // foreach ($userInvoicesDates as $key => $value) {
+    //     $newKey  = $value['DocDate'];
+    //     if (isset($otherArray[$newKey])) {
+    //         $otherArray[$newKey][] = $key;
+    //     } else {
+    //         $kwys = [];
+    //         $kwys[] = $key;
+    //         $otherArray[$newKey]  = $kwys;
+    //     }
+    // }
+    // ksort($otherArray);
+    // $newArrayForDate = [];
+    // $newContainer = [];
+    // foreach ($otherArray as $date => $arrOfEntries) {
+    //     foreach ($arrOfEntries as $index => $docEntry) {
+    //         $newArrayForDate[$docEntry] = [
+    //             'Total' => AlJouaiRequests::getInvoiceDocTotal($docEntry),
+    //             'NumberOfItems' => AlJouaiRequests::getCountOfNumbers($docEntry),
+    //             'Dates' => AlJouaiRequests::getInvoiceDatesOnly($docEntry),
+    //         ];
+    //         if (isset($newContainer[$date])) {
+    //             $newContainer[$date][$docEntry][] = $newArrayForDate[$docEntry];
+    //         } else {
+    //             $newContainer[$date] = [];
+    //             $newContainer[$date][$docEntry][] = $newArrayForDate[$docEntry];
+    //         }
+    //     }
+    // }
+    return response()->json([]); // Data is Now Sorted 
+    // return response()->json($newContainer); // Data is Now Sorted 
 }); // * DONE 
